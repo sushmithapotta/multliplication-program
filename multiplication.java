cb6298ca558f9aa92d70d33533c0cc0a0512a6b8
sushmithapotta <pottasushmitha9@gmail.com>
2025-01-30 14:03:25 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,7 @@
+public class multiplication {
+    public static void main(String[] args) {
+        int a=6;
+        int b=7;
+        System.out.printf("multiple of %d * %d : %d",a,b,a*b);
+    }
+}
